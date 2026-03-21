9801bda9137dc5975094fdf125c1e27551802092
SpicyMomo <spicymomo@MacBook-Air-de-SpicyMomo.local>
2026-03-21 22:21:57 +0100

    push

--- parsing.c
+++ parsing.c
@@ -14,13 +14,29 @@
 
 int	main(int ac, char **ag)
 {
+	int	i;
+	int	j;
+
+	i = 0;
 	if (ac < 2)
 	{
 		write(1, "Error\n", 6);
-		return (0);
-	}
-	if (ac >= 2
+		return (1);
+	}	
+	while (i < ac)
 	{
-		
+		j = 0;
+		while (*ag[j])
+		{
+			if (*ag[j] >= '0' &&  *ag[j] <= '9')
+			{
+				write(1, "Error\n", 6);
+				return (1);
+			}
+			else
+				write(1, "OK", 1);
+			j++;
+		}
+		i++;
 	}	
 }
